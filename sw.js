5274c37779c2cac3c66be5ff66ffc8b7e9b8f2fe
dhon1979 <91603101+dhon1979@users.noreply.github.com>
2026-07-05 20:31:05 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const C = 'gasoil-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(C).then(c => c.addAll(['./', './index.html'])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => self.clients.claim());
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      const cl = res.clone();
+      caches.open(C).then(c => c.put(e.request, cl));
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
