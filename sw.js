af8c32c86786f7a5f6e03c114017daff1ae98330
dhon1979 <91603101+dhon1979@users.noreply.github.com>
2026-07-06 12:31:14 -0300

    Upgrade service worker cache version to gasoil-v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'gasoil-v1';
+const C = 'gasoil-v2';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(C).then(c => c.addAll(['./', './index.html'])));
   self.skipWaiting();
